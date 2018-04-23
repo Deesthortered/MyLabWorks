aa42b057e310b1a0f3f5ac56d57365603e7c0c24
Deesthortered <anatoly.davydko@gmail.com>
2018-04-23 17:23:00 +0300

    All needed thing were included and connected to project. Let's start.

--- Lab2/MyForm.cpp
+++ Lab2/MyForm.cpp
@@ -1,2 +1,10 @@
 #include "MyForm.h"
+#include <windows.h>
+using namespace Lab2;
 
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew MyForm);
+}
--- Lab2/MyForm.h
+++ Lab2/MyForm.h
@@ -1,7 +1,10 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <WinSock2.h>
+#pragma comment(lib, "WS2_32.lib")
 
-namespace Lab2 {
-
+namespace Lab2 
+{
 	using namespace System;
 	using namespace System::ComponentModel;
 	using namespace System::Collections;
@@ -9,51 +12,39 @@ namespace Lab2 {
 	using namespace System::Data;
 	using namespace System::Drawing;
 
-	/// <summary>
-	/// —водка дл€ MyForm
-	/// </summary>
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
-	public:
-		MyForm(void)
+
+
+	public:	MyForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: добавьте код конструктора
-			//
 		}
-
-	protected:
-		/// <summary>
-		/// ќсвободить все используемые ресурсы.
-		/// </summary>
-		~MyForm()
+	protected: ~MyForm()
 		{
 			if (components)
 			{
 				delete components;
 			}
 		}
-
-	private:
-		/// <summary>
-		/// ќб€зательна€ переменна€ конструктора.
-		/// </summary>
-		System::ComponentModel::Container ^components;
-
+	private: System::ComponentModel::Container ^components;
 #pragma region Windows Form Designer generated code
-		/// <summary>
-		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
-		/// содержимое этого метода с помощью редактора кода.
-		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(806, 532);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+
+
 	};
 }
